f79f8c721c381e2f14223492e19064292e8ac5d3
Bart Oostveen <bart@bartoostveen.nl>
2026-07-07 21:18:35 +0200

    fix(deps): fix regression in requirement loading

--- roundcube_oidc.php
+++ roundcube_oidc.php
@@ -1,7 +1,7 @@
 <?php
 
 // Require composer autoload for direct installs
-@include __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 use Jumbojett\OpenIDConnectClient;
 
